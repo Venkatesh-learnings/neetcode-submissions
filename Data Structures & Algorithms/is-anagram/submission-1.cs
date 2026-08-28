bc09dcf3fe46cd95777324396fa7ae7bd2ad4f24
Venkatesh-learnings <venkateshnadakudhiti.learnings@gmail.com>
2026-08-28 07:49:55 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cs
@@ -0,0 +1,19 @@
+
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+    
+    if(s.Length!=t.Length)
+     return false;
+    Dictionary<char,int> freq=new();
+    foreach(char c in s.ToLower())
+        freq[c]=freq.GetValueOrDefault(c,0)+1;
+    foreach(char c in t.ToLower())
+    {
+        freq[c]=freq.GetValueOrDefault(c,0)-1;
+        if(freq[c]<0){
+            return false;
+        }
+    }
+     return freq.Values.All(v=>v==0);
+}
+}
